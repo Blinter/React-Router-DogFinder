029eeb503131daaae89d6824b092e0b4c61f71e7
Blinter <90986012+Blinter@users.noreply.github.com>
2024-10-18 22:39:07 -0400

    fix: 404 from GH deploy environment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: 'https://blinter.github.io/React-Router-DogFinder/'
+  base: '/React-Router-DogFinder/',
+  server: {
+    middlewareMode: 'html'
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        '404': resolve(__dirname, '404.html')
+      }
+    }
+  }
 })
